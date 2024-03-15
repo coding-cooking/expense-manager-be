31673a117a0b16112b6abee3892e62301f327c22
Coding-Cooking <fei.coding2023@gmail.com>
2024-03-15 21:21:55 +1100

    Remove ignored files from the repository

--- config/prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	URI: '',
-	SECRET: '', // add secret here. This can be random set of letters
-	GITHUB_CLIENT_ID: '', // add client id here
-	GITHUB_CLIENT_SECRET: '', //add client secret here
-	GITHUB_CALLBACK_URL: '',
-};
